c3372d3007ef5e54ecf8cf472c09634e7c26f6dd
Marufa2018 <45197802+Marufa2018@users.noreply.github.com>
2020-09-08 21:20:54 +0600

    Add files via upload

--- deployment/eks_cluster.sh
+++ deployment/eks_cluster.sh
@@ -1,1 +1,1 @@
-eksctl create cluster --name pod --region us-east-2 --version 1.17 --nodegroup-name workers --node-type t2.micro --nodes 3 --nodes-min 1 --nodes-max 4 --node-ami auto
+eksctl create cluster --name pod --region us-east-2 --version 1.14 --nodegroup-name workers --node-type t2.micro --nodes 3 --nodes-min 1 --nodes-max 4 --node-ami auto
